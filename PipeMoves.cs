815ea8df36f85879de8575084b535c10a35cbbc4
SBhatia772 <71363508+SBhatia772@users.noreply.github.com>
2020-11-22 12:12:02 -0500

    Add files via upload
    The Pipe Moves script is used for both the pipe and game objects in the game.

---
+++ PipeMoves.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoves : MonoBehaviour {
+
+    public float speed;
+    ObstacleSpawner pipeSpawner = new ObstacleSpawner();
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    void speedCheck()
+    {
+        if (Score.score >= 5 && Score.score < 6)
+        {
+            speed += 0.01f;
+            pipeSpawner.maxTime -= 0.5f;
+        }
+    }
+   
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //speedCheck();
+        transform.position += Vector3.left * speed * Time.deltaTime;
+	}
+
+    
+
+}
